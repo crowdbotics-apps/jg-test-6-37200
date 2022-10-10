dc415c62e7ac2fdc7aa8ef0e703b8e308fa5e3c3
Crowdbotics <team@crowdbotics.com>
2022-10-10 01:06:59 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,15 @@
+from django.conf import settings
 from django.db import models
+
+
+class Services(models.Model):
+    "Generated Model"
+    service = models.CharField(
+        max_length=256,
+    )
+    status = models.CharField(
+        max_length=256,
+    )
+    createdDate = models.DateField()
+    terminatedDate = models.DateField()
+    description = models.TextField()
